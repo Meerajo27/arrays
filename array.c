316ef5fabfe4c4468c31905648bddfe80d28f0a7
Meerajo27 <79192444+Meerajo27@users.noreply.github.com>
2021-05-29 18:15:03 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+  int arr1[3][3],i,j,sum=0;
+  
+       printf("\n\nRead a 2D array of size 3x3 and print the matrix :\n");
+       printf("------------------------------------------------------\n");  
+  
+ 
+    /* Stored values into the array*/
+       printf("Input elements in the matrix :\n");
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+	      printf("element - [%d],[%d] : ",i,j);
+	      scanf("%d",&arr1[i][j]);
+      }
+  }  
+ 
+ printf("\nThe matrix is : \n");
+  for(i=0;i<3;i++)
+  {
+      printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr1[i][j]);
+  }
+ printf("\n\n");
+ for(i=0;i<3;i++)
+ {
+ sum= sum+arr1[i][i];
+}
+printf("\nsum of diagonal elements of a matrix = %d",sum);
+ 
+ 
+}
